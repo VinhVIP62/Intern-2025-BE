4f6e89f5ec07a7c0044d220e4e52990c3a2c34e8
ABsievil <nccuong317qb@gmail.com>
2025-06-11 16:09:49 +0700

    docs(env.config): config env for jwt variable

--- src/infrastructure/config/env.config.ts
+++ src/infrastructure/config/env.config.ts
@@ -4,4 +4,9 @@ export default () => ({
   database: {
     uri: process.env.DATABASE_URI || 'mongodb://localhost:27017/sport-hub',
   },
-}); 
+  jwt: {
+    secret: process.env.JWT_SECRET || 'secret',
+    accessTokenExpiration: process.env.JWT_ACCESS_TOKEN_EXPIRATION || '15m',
+    refreshTokenExpiration: process.env.JWT_REFRESH_TOKEN_EXPIRATION || '7d',
+  },
+});
