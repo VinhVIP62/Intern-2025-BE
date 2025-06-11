4ea92b3503e8b95194a25f726720a9ff64d396ce
ABsievil <nccuong317qb@gmail.com>
2025-06-11 09:44:35 +0700

    docs(main.ts): config Router versioning

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@src/app.module';
 import { WinstonLoggerService } from '@infrastructure/logger/winston.logger.service';
+import { VersioningType, RequestMethod } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: new WinstonLoggerService(),
   });
+
+  // Enable versioning
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'v',
+  });
+
+  // Set global prefix
+  app.setGlobalPrefix('api', {
+    exclude: [{ path: '/', method: RequestMethod.GET }],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
