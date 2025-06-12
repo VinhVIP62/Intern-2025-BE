de06e7a250cfaec6b005573aa52e08ac41c54b65
quydoan0316 <quydoan0316@gmail.com>
2025-06-12 16:41:10 +0700

    feat(validator): handling validator for user dto

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { IEnvVars } from '@configs/config';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { VersioningType } from '@nestjs/common';
 import { WinstonLogger } from './common/logger/winston.logger';
+import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 async function bootstrap() {
 	const app = await NestFactory.create<NestExpressApplication>(AppModule, {
@@ -22,6 +23,11 @@ async function bootstrap() {
 		type: VersioningType.URI,
 	});
 
+	app.useGlobalPipes(new ValidationPipe({
+		whitelist: true,
+		forbidNonWhitelisted: true,
+		transform: true,
+  	}));
 	await app.listen(configService.get('port', { infer: true })!);
 }
 
