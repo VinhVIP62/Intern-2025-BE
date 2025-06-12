bedbbc20f8afdee1c2ed77dae0e4ff7e51d7e006
D-ng1277 <haidangbachkhoa@gmail.com>
2025-06-12 13:03:03 +0700

    fix: improved path imports and logger config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import config from '@configs/config';
-import { AuthModule } from './modules/auth/auth.module';
-import { AdminModule } from './modules/admin/admin.module';
-import { EventModule } from './modules/event/event.module';
-import { NotificationModule } from './modules/notification/notification.module';
-import { PostModule } from './modules/post/post.module';
-import { UserModule } from './modules/user/user.module';
+import {
+	AuthModule,
+	AdminModule,
+	EventModule,
+	NotificationModule,
+	PostModule,
+	UserModule,
+} from './modules';
 
 @Module({
 	imports: [
--- src/common/logger/winston.logger.ts
+++ src/common/logger/winston.logger.ts
@@ -1,15 +1,21 @@
-import { WinstonModule } from 'nest-winston';
+import { utilities, WinstonModule } from 'nest-winston';
 import * as winston from 'winston';
 
 export const WinstonLogger = WinstonModule.createLogger({
-  transports: [
-    new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.printf(({ timestamp, level, message }) => {
-          return `[${String(timestamp)}] ${String(level).toUpperCase()}: ${String(message)}`;
-        }),
-      ),
-    }),
-  ],
+	transports: [
+		new winston.transports.Console({
+			format: winston.format.combine(
+				winston.format.timestamp(),
+				winston.format.printf(({ timestamp, level, message }) => {
+					return `[${String(timestamp)}] ${String(level).toUpperCase()}: ${String(message)}`;
+				}),
+				utilities.format.nestLike('MyApp', {
+					colors: true,
+					prettyPrint: true,
+					processId: true,
+					appName: true,
+				}),
+			),
+		}),
+	],
 });
