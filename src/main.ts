655539ae4dc4089f0a527f8ae792721b07a79a64
Trihoang <96430986+tri1234512321@users.noreply.github.com>
2025-06-16 10:55:43 +0700

    add router, fix logger, add logger for exeption handling

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,16 @@ import { ConfigService } from '@nestjs/config';
 import { IEnvVars } from '@configs/config';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { VersioningType } from '@nestjs/common';
-import { WinstonLogger } from './common/logger/winston.logger';
 import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
+import { AppLoggerService } from '@common/logger/logger.service';
 
 async function bootstrap() {
 	const app = await NestFactory.create<NestExpressApplication>(AppModule, {
 		bufferLogs: true,
-		logger: WinstonLogger,
 	});
+
+	const logger = app.get(AppLoggerService);
+	app.useLogger(logger);
 
 	const configService = app.get(ConfigService<IEnvVars>);
 
