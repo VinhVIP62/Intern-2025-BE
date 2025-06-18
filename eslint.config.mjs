ff38803573362922c82f56db79bd0f5b3f161145
D-ng1277 <haidangbachkhoa@gmail.com>
2025-06-18 17:27:17 +0700

    chore: added no-console rule eslint.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,6 +27,7 @@ export default tseslint.config(
   },
   {
     rules: {
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
